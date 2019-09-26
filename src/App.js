af6570387868da67a5a4612767b80949aef77ecd
Simon mahns <ak.simonm@gmail.com>
2019-09-26 09:59:05 -0500

    publish

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//https://codepen.io/enesser/pen/jdenE
+//https://codepen.io/guillaumerxl/pen/pjbYoB
+//https://codepen.io/kenjiSpecial/pen/wavooR
+
+//https://tympanus.net/codrops/2019/01/17/interactive-particles-with-three-js/
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
